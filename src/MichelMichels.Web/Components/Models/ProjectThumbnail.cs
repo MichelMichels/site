b121dfe79a8095e4e9fb1b877ed8f38ba2ca7acb
Michel Michels <michel@miche.ls>
2024-05-02 14:26:55 +0200

    Add extra button for BpostSharp

---
+++ src/MichelMichels.Web/Components/Models/ProjectThumbnail.cs
@@ -0,0 +1,9 @@
+﻿namespace MichelMichels.Web.Components.Models;
+
+public class ProjectThumbnail
+{
+    public string Emoji { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Link { get; set; } = string.Empty;
+}
---
+++ src/MichelMichels.Web/Components/Pages/Projects/Projects.razor.cs
@@ -0,0 +1,22 @@
+﻿using MichelMichels.Web.Components.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace MichelMichels.Web.Components.Pages.Projects;
+
+public partial class Projects : ComponentBase
+{
+    public List<ProjectThumbnail> Cards { get; set; } = [
+        new ProjectThumbnail {
+            Emoji = "🛂",
+            Title = "ViesSharp",
+            Description = "C# API client to validate VAT numbers of European member states through VOW.",
+            Link = "/projects/vies"
+        },
+        new ProjectThumbnail {
+            Emoji = "📯",
+            Title = "BpostSharp",
+            Description = "C# service to obtain city data provided by the Belgian postal service.",
+            Link = "http://git.miche.ls/BpostSharp"
+        }
+    ];
+}
