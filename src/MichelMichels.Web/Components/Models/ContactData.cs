ba73cf24ab162efe41ce530fa2c66dd24f119d08
Michel Michels <michel@miche.ls>
2024-05-28 22:18:57 +0200

    Add contact data

---
+++ src/MichelMichels.Web/Components/Models/ContactData.cs
@@ -0,0 +1,7 @@
+﻿namespace MichelMichels.Web.Components.Models;
+
+public class ContactData
+{
+    public string Link { get; set; } = "#";
+    public string IconClass { get; set; } = string.Empty;
+}
---
+++ src/MichelMichels.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,36 @@
+﻿using MichelMichels.Web.Components.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace MichelMichels.Web.Components.Pages;
+
+public partial class Home : ComponentBase
+{
+    private Random random = new();
+
+    public List<ContactData> ContactOptions { get; set; } = [];
+
+    public string Name { get; set; } = "Michel Michels";
+
+    protected override void OnInitialized()
+    {
+        ContactOptions = [
+            new ContactData {
+                Link = "mailto:michel@miche.ls",
+                IconClass = "bi-envelope-at",
+            },
+            new ContactData {
+                Link = "https://www.linkedin.com/in/michelmichelsjr/",
+                IconClass = "bi-linkedin",
+            },
+            new ContactData {
+                Link = "https://github.com/MichelMichels/",
+                IconClass = "bi-github",
+            }
+        ];
+    }
+
+    private string GetRandomColorClass()
+    {
+        return $"c-4-{random.Next(16)}";
+    }
+}
